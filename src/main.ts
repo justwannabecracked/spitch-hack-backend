bc77d09ac0b78b6b0e9ff494f9d6642f076bd20b
Femi Obadimu <ficazzosam@gmail.com>
2025-09-13 01:24:40 +0100

    update application

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://spitch-hack-backend.onrender.com',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+    // origin: 'https://spitch-hack-backend.onrender.com',
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // credentials: true,
   });
   await app.listen(process.env.PORT ?? 8000);
 }
