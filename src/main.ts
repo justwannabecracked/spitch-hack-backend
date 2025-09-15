2ebcc86321636b8c3165a5fff3b0c1b1aae494fd
Femi Obadimu <ficazzosam@gmail.com>
2025-09-15 17:30:43 +0100

    Ready to Go

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '500mb' }));
   app.enableCors({
     // origin: 'https://spitch-hack-backend.onrender.com',
     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
